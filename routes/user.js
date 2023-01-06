e17dd1c9de724aa964b427a4ca6dedc869282bb3
tanaybasak <tanoybasak@gmail.com>
2023-01-06 11:12:38 +0530

    changes

--- routes/user.js
+++ routes/user.js
@@ -10,13 +10,13 @@ router.get('/googlelogin', passport.authenticate('google', {
 }));
 
 
-router.get('/login', passport.authenticate('google'),
-    (req, res, next) => {
-        res.send('Logged In');
-    })
+router.get('/login', passport.authenticate('google', {
+    successRedirect: process.env.FRONTEND_URL
+})
+);
 
 router.get('/logout', logout);
-router.get('/me', isAuthenticated, myProfile)
+router.get('/me', isAuthenticated, myProfile);
 router.get('/admin/users', isAuthenticated, authorizeAdmin, getAdminUsers);
 router.get('/admin/stats', isAuthenticated, authorizeAdmin, getAdminStats);
 export default router;
